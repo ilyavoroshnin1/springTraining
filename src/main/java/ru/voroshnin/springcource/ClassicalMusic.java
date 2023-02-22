50879f648c67888de47e6d8c058d8aacfc88a059
ilyavoroshnin1 <ilja.144@mail.ru>
2023-02-22 18:11:16 +0300

    Test2

---
+++ src/main/java/ru/voroshnin/springcource/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package ru.voroshnin.springcource;
+
+public class ClassicalMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Rhapsody";
+    }
+}
---
+++ src/main/java/ru/voroshnin/springcource/RockMusic.java
@@ -0,0 +1,8 @@
+package ru.voroshnin.springcource;
+
+public class RockMusic implements Music{
+    @Override
+    public String getSong() {
+        return "In the end";
+    }
+}
